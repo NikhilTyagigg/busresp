8878d57682fac133ebf3a4c70054211dab266795
Nikhil <nikhiltyagi.n17@gmail.com>
2024-05-23 16:13:57 +0530

    last

--- OnboardServer/app.js
+++ OnboardServer/app.js
@@ -1,63 +1,63 @@
-var express = require('express');
-var path = require('path');
+var express = require("express");
+var path = require("path");
 const cors = require("cors");
-var dotenv = require('dotenv')
-const logger = require('./helper/logger')
-var mLogger = require('morgan');
-const mqtt = require('./helper/mqtt')
-dotenv.config()
-var swaggerUi = require('swagger-ui-express'),
-    swaggerDocument = require('./swagger.json');
+var dotenv = require("dotenv");
+const logger = require("./helper/logger");
+var mLogger = require("morgan");
+const mqtt = require("./helper/mqtt");
+dotenv.config();
+var swaggerUi = require("swagger-ui-express");
+var swaggerDocument = require("./swagger.json");
 
-
-var app = express('');
+var app = express("");
 
 logger.stream = {
-  write: function(message, encoding){
-      logger.info(message);
-  }
+  write: function (message, encoding) {
+    logger.info(message);
+  },
 };
-app.use(mLogger('combined', { stream: logger.stream }));
-
-
+app.use(mLogger("combined", { stream: logger.stream }));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 app.use(cors());
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({  
-  limit: '50mb',
-  extended: true,
-  parameterLimit : 50000
-}));
+app.use(express.json({ limit: "50mb" }));
+app.use(
+  express.urlencoded({
+    limit: "50mb",
+    extended: true,
+    parameterLimit: 50000,
+  })
+);
 
-const apiV1Router = require('./routes/api/v1');
+const apiV1Router = require("./routes/api/v1");
 
-app.use(express.static(path.join(__dirname, '/build')));
+app.use(express.static(path.join(__dirname, "/build")));
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
+app.use("/api/v1", apiV1Router);
 
-app.use('/api/v1', apiV1Router);
-
-app.get('/', function(req, res, next) {
-    //return res.sendFile(__dirname, 'build','index.html');
-        return res.sendFile(path.join(__dirname, 'build/index.html'));
-})
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.get("/", function (req, res, next) {
+  return res.sendFile(path.join(__dirname, "build/index.html"));
 });
 
+// error handler
+app.use(function (err, req, res, next) {
+  // only providing error in development
+  const isDevelopment = req.app.get("env") === "development";
+  const errorResponse = {
+    message: err.message,
+    ...(isDevelopment && { error: err }), // only include error details in development
+  };
 
-app.get('/*', function(req, res, next) {
-  return res.sendFile(__dirname + '/build/index.html');
-})
+  res.status(err.status || 500).json(errorResponse);
+});
 
-mqtt.connectToMqtt()
+app.get("/*", function (req, res, next) {
+  return res.sendFile(path.join(__dirname, "build/index.html"));
+});
+
+mqtt.connectToMqtt();
+
 module.exports = app;
--- OnboardServer/routes/api/v1/index.js
+++ OnboardServer/routes/api/v1/index.js
@@ -1,61 +1,75 @@
-var express = require('express');
-var router = express.Router();
-const logger = require('../../../helper/logger');
-const db = require('../../../models');
-const { ErrorCodes, NotFoundError, ApplicationError, UserError } = require('../../../helper/errors');
-const { checkAccess} = require('../../../auth/jwt')
+const express = require("express");
+const router = express.Router();
+const logger = require("../../../helper/logger");
+const db = require("../../../models");
+const {
+  ErrorCodes,
+  NotFoundError,
+  ApplicationError,
+  UserError,
+} = require("../../../helper/errors");
+const { checkAccess } = require("../../../auth/jwt");
 const Sequelize = db.Sequelize;
 const errorBody = (err) => ({ code: err.code, message: err.message });
 
-const authRouter = require('./auth');
-const routeRouter = require('./routeConfig')
+const authRouter = require("./auth");
+const routeRouter = require("./routeConfig");
 
-router.use('/auth', authRouter);
-router.use('/route', checkAccess ,routeRouter);
-
+router.use("/auth", authRouter);
+router.use("/route", checkAccess, routeRouter);
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
 });
 
-// handle all not found errors
+// Handle all not found errors
 router.use(function (req, res, next) {
-    const err = new NotFoundError(req.url);
-    logger.error(err + ' USER ID');
-    res.status(404).send(errorBody(err));
+  const err = new NotFoundError(req.url);
+  logger.error(`${err} USER ID`);
+  res.status(404).send(errorBody(err));
 });
 
-// error handler
+// Error handler
 router.use(function (err, req, res, next) {
-    // set locals, only providing error in development
-    logger.error(err);
-    if (err instanceof Sequelize.ForeignKeyConstraintError) {
-        logger.alert(err);
-        err = new UserError('This resource is being used by some other resource', ErrorCodes.ERR_VALIDATION);
-    } else if (err instanceof Sequelize.ValidationError) {
-        logger.alert(err);
-        err = new UserError('validation failed', ErrorCodes.ERR_VALIDATION);
-    } else if (err instanceof Sequelize.DatabaseError) {
-        logger.alert(err);
-        err = new UserError('validation failed', ErrorCodes.ERR_DATABASE, 500);
-    } else if (err instanceof Sequelize.TimeoutError) {
-        logger.alert(err);
-        err = new UserError('database query timeout', ErrorCodes.ERR_DATABASE_TIMEOUT, 500);
-    } else if (err instanceof Sequelize.UniqueConstraintError) {
-        logger.alert(err);
-        err = new UserError('already existis', ErrorCodes.ERR_VALIDATION);
-    } else if (err instanceof Sequelize.Error) {
-        logger.alert(err);
-        err = new UserError('Something went wrong!!');
-    } else if ((err instanceof Error && err.name === 'ValidationError')
-        || (err instanceof ApplicationError)) {
-        err = err;
-    } else {
-        logger.alert(err);
-        err = new UserError(err?.message ? err.message : 'Something went wrong!!');
-    }
-    res.status(err.httpCode || 400).send(errorBody(err));
+  logger.error(err);
+
+  if (err instanceof Sequelize.ForeignKeyConstraintError) {
+    logger.alert(err);
+    err = new UserError(
+      "This resource is being used by some other resource",
+      ErrorCodes.ERR_VALIDATION
+    );
+  } else if (err instanceof Sequelize.ValidationError) {
+    logger.alert(err);
+    err = new UserError("Validation failed", ErrorCodes.ERR_VALIDATION);
+  } else if (err instanceof Sequelize.DatabaseError) {
+    logger.alert(err);
+    err = new UserError("Database error", ErrorCodes.ERR_DATABASE, 500);
+  } else if (err instanceof Sequelize.TimeoutError) {
+    logger.alert(err);
+    err = new UserError(
+      "Database query timeout",
+      ErrorCodes.ERR_DATABASE_TIMEOUT,
+      500
+    );
+  } else if (err instanceof Sequelize.UniqueConstraintError) {
+    logger.alert(err);
+    err = new UserError("Already exists", ErrorCodes.ERR_VALIDATION);
+  } else if (err instanceof Sequelize.Error) {
+    logger.alert(err);
+    err = new UserError("Something went wrong!!");
+  } else if (
+    (err instanceof Error && err.name === "ValidationError") ||
+    err instanceof ApplicationError
+  ) {
+    // leave the error as is
+  } else {
+    logger.alert(err);
+    err = new UserError(err?.message || "Something went wrong!!");
+  }
+
+  res.status(err.httpCode || 400).send(errorBody(err));
 });
 
 module.exports = router;
